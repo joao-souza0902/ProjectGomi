6ef127995a10bce20700975f2f456a3846fe38eb
Rizukykun <guilhermebarrosrl@gmail.com>
2021-05-14 14:16:14 -0300

    Adicionada a controladora de Login e Dao de Login, juntamente do arquivo de validações no business

---
+++ br.com.gomi.front/src/br/com/gomi/front/Controllers/LoginController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.gomi.front.Controllers;
+import br.com.gomi.business.*;
+import br.com.gomi.shared.UsuarioViewModel;
+import java.io.IOException;
+import java.sql.SQLException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Administrador
+ */
+public class LoginController
+{
+    @FXML
+    private TextField loginTextField;
+    private TextField senhaTextField;
+    
+    public void btnLoginOnClick(ActionEvent event) throws IOException, SQLException {
+        
+        Parent home_page_parent = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+        Scene home_page_scene = new Scene(home_page_parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_page_scene);
+        
+        if (Validacao.validaLogin(loginTextField.getText(), senhaTextField.getText()))
+            app_stage.show();
+        else
+            System.out.println("Login Inválido!");
+    }
+}
